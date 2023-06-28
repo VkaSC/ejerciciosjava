58dfa474cb5c2f3700126667456bdfc5d383b88b
Veronica Sanchez Camacho <vsc1972@gmail.com>
2023-06-28 14:48:46 +0200

    Clase 28/06 Gestion de errores centralizado terminada capa rest

--- restaurantesmalagaapp/src/main/java/edu/arelance/nube/controller/RestauranteController.java
+++ restaurantesmalagaapp/src/main/java/edu/arelance/nube/controller/RestauranteController.java
@@ -1,85 +1,119 @@
 package edu.arelance.nube.controller;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.arelance.nube.repository.entity.Restaurante;
 import edu.arelance.nube.service.RestauranteService;
+import io.swagger.v3.oas.annotations.Operation;
 
 //import org.springframework.stereotype.Controller;
 
-
-
 /**
- * API WEB
- * HTTP -> Deriva en la ejecución de un método
+ * API WEB HTTP -> Deriva en la ejecución de un método
  * 
- * GET -> consultar todos
- * GET -> consultar por id (uno)
- * POSt -> insertar 
- * PUT -> Modifica un registro que ya existe
- * Delete -> Borra un registro (por Id)
- * GET -> Busqueda: por barrio, por nombre, por especialidad o precio Medio
+ * GET -> consultar todos GET -> consultar por id (uno) POSt -> insertar PUT ->
+ * Modifica un registro que ya existe Delete -> Borra un registro (por Id) GET
+ * -> Busqueda: por barrio, por nombre, por especialidad o precio Medio
  * 
- * Recibe y devuelve información, pero no hace nada con ella, la manipulación pertenece a service
+ * Recibe y devuelve información, pero no hace nada con ella, la manipulación
+ * pertenece a service
  */
 //@Controller   => Es un controlador menos específico devuelve una representación (mas antiguo)
-@RestController //Return JSon
+@RestController // Return JSon
 @RequestMapping("/restaurante")
 public class RestauranteController {
-	
-	//Atributo
-	@Autowired
+
+	// Atributo
+	@Autowired // inyeccion de dependencias
 	RestauranteService restauranteService;
-	
-	
-	//Metodos
-	@GetMapping("/test") //Get http://localhost:8081/restaurante/test
+
+	// Metodos
+	@GetMapping("/test") // Get http://localhost:8081/restaurante/test
 	public Restaurante obtenerRestauranteTest() {
-		
+
 		Restaurante restaurante = null;
 		System.out.println("LLamando a obtenerRestauranteTest");
-		restaurante = new Restaurante(1l, "Martinete", "carlos haya 33", "carranque", "www.martinete.org", "http://gogle.xe", 33.65f, -2.3f, 10, "gazpachelo", "paella", "sopa de marisco",  LocalDateTime.now());
+		restaurante = new Restaurante(1l, "Martinete", "carlos haya 33", "carranque", "www.martinete.org",
+				"http://gogle.xe", 33.65f, -2.3f, 10, "gazpachelo", "paella", "sopa de marisco", LocalDateTime.now());
 		return restaurante;
 	}
-	
+
 	// GET -> consultar todos -> http://localhost:8081/restaurante
-		//ResponseEntity devuelve un mensaje http completo, se pone '?' para hacer el tipo de dato genérico
+	// ResponseEntity devuelve un mensaje http completo, se pone '?' para hacer el
+	// tipo de dato genérico
 	@GetMapping
-	public ResponseEntity<?> obtenerRestaurantes(){
-		
+	public ResponseEntity<?> obtenerRestaurantes() {
+
 		ResponseEntity<?> responseEntity = null;
 		Iterable<Restaurante> lista_restaurantes = null;
-		
-			lista_restaurantes = this.restauranteService.consultarTodos();
-			responseEntity = ResponseEntity.ok(lista_restaurantes);
-			
+
+		lista_restaurantes = this.restauranteService.consultarTodos();
+		responseEntity = ResponseEntity.ok(lista_restaurantes); //Mensaje de respuesta
+
 		return responseEntity;
 	}
-	// * GET -> consultar por id  http://localhost:8081/restaurante/5
+
+	// * GET -> consultar por id http://localhost:8081/restaurante/5
+	@Operation(description = "Este servicio permite la consulta de restaurante por id")
 	@GetMapping("/{id}")
-	public ResponseEntity<?> listarPorId(@PathVariable Long id){
-		
+	public ResponseEntity<?> listarPorId(@PathVariable Long id) {
+
 		ResponseEntity<?> responseEntity = null;
-			
+		Optional<Restaurante> or = null;
+		or = this.restauranteService.consultarUno(id);
+		if ( or.isPresent()) {
+			//La consulta ha encontrado registro
+			Restaurante restauranteLeido = or.get();
+			responseEntity = ResponseEntity.ok(restauranteLeido);
+		} else {
+			//La consulta no ha encontrado registro
+			responseEntity = ResponseEntity.noContent().build();
+		};
+
 		return responseEntity;
 	}
-	// * POST -> insertar  http://localhost:8081/restaurante (Body Restaurante)
-	// * PUT -> Modifica un registro que ya existe  http://localhost:8081/restaurante/id (Body Restaurante)
-	
-	// * Delete -> Borra un registro (por Id)  http://localhost:8081/restaurante/3
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> borrarPorId(@PathVariable Long id){
-		
+
+	// * POST -> insertar http://localhost:8081/restaurante (Body Restaurante)
+	@PostMapping
+	public ResponseEntity<?> insertarRestaurante(@RequestBody Restaurante restaurante) {
+
 		ResponseEntity<?> responseEntity = null;
-			
+		Restaurante restauranteNuevo = null;
+		restauranteNuevo = this.restauranteService.altaRestaurante(restaurante);
+		responseEntity = ResponseEntity.status(HttpStatus.CREATED).body(restauranteNuevo);
+		return responseEntity;
+	}
+
+	// * PUT -> Modifica un registro que ya existe
+	// http://localhost:8081/restaurante/id (Body Restaurante)
+	@PutMapping("/{id}")
+	public ResponseEntity<?> modificarRestaurante(@RequestBody Restaurante restaurante, @PathVariable Long id) {
+
+		ResponseEntity<?> responseEntity = null;
+		return responseEntity;
+	}
+
+	// * Delete -> Borra un registro (por Id) http://localhost:8081/restaurante/3
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> borrarPorId(@PathVariable Long id) {
+
+		ResponseEntity<?> responseEntity = null;
+		this.restauranteService.borrarRestaurante(id);
+		responseEntity = ResponseEntity.ok().build();
+
 		return responseEntity;
 	}
 
--- restaurantesmalagaapp/src/main/java/edu/arelance/nube/service/RestauranteServiceImpl.java
+++ restaurantesmalagaapp/src/main/java/edu/arelance/nube/service/RestauranteServiceImpl.java
@@ -2,16 +2,18 @@ package edu.arelance.nube.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.arelance.nube.repository.RestauranteRepository;
 import edu.arelance.nube.repository.entity.Restaurante;
 
-@Service
+@Service //crea la instancia de RestauranteServiceImpl
 public class RestauranteServiceImpl implements RestauranteService{
 	
 	//Atributo de la clae que debe comvertirse en una instancia que necesita para comunicarse con el repositorio
+	@Autowired //nos trae la instancia desde el contexto.
 	RestauranteRepository restauranteRepository;
 
 	
